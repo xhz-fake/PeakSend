46df18e0288f74f65ca00dfc65274b0047bb3096
邢浩哲（MySQL Vue JDBC） <oncwnuGXunUkVa40y3ao68niiOOQ@git.weixin.qq.com>
2026-05-14 20:07:56 +0800

    feat(Day1):实现员工登录、新增及重复校验功能

--- peaksend-backend/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ peaksend-backend/sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,17 +1,23 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
+import com.sky.exception.BaseException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -39,7 +45,8 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
+        // 对前端传过来的密码进行md5加密
+        password = DigestUtils.md5DigestAsHex(password.getBytes());
         if (!password.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
@@ -52,6 +59,40 @@ public class EmployeeServiceImpl implements EmployeeService {
 
         //3、返回实体对象
         return employee;
+    }
+
+    /**
+     *
+     * @param employeeDTO
+     */
+    public void save(EmployeeDTO employeeDTO) {
+        Employee existingEmployee = employeeMapper.getByUsername(employeeDTO.getUsername());
+        if (existingEmployee != null) {
+            throw new BaseException(employeeDTO.getUsername() + MessageConstant.ALREADY_EXISTS);
+        }
+
+        Employee employee = new Employee();
+
+        //  这里使用对象属性拷贝来减少不必要的代码
+        BeanUtils.copyProperties(employeeDTO,employee);//从前端返回的 DTO 中拷贝到我们的实体 employee 中，要求两边字段的命名一致，
+
+        // 对于前者没有的字段值我们单独进行赋值
+        employee.setStatus(StatusConstant.ENABLE);// 不硬编码
+
+        //设置默认密码(md5加密的)
+        employee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+
+        // 设置当前记录的创建时间和修改时间
+        employee.setCreateTime(LocalDateTime.now());
+        employee.setUpdateTime(LocalDateTime.now());
+
+        // 设置当前记录创建人 id 及修改人 id
+        // TODO 后期需要改为当前登录用户的 id，避免写死
+        employee.setCreateUser(10L);
+        employee.setUpdateUser(10L);
+
+        employeeMapper.insert(employee);
+
     }
 
 }
